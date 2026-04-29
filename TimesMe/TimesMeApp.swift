da8dc9c74543ed2f7d627afd71bcc00596c847a8
= <douglasdwebb@msn.com>
2026-04-29 08:24:17 -0400

    Initial Commit

---
+++ TimesMe/TimesMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesMeApp.swift
+//  TimesMe
+//
+//  Created by Douglas Webb on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TimesMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
