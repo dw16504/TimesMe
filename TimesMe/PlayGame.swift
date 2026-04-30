aa1aaf34d1177ff3104f491e7638068c3535bf9d
= <douglasdwebb@msn.com>
2026-04-30 07:25:14 -0400

    initial commit, seetting screen set up, just before conditionals on table selector

---
+++ TimesMe/PlayGame.swift
@@ -0,0 +1,25 @@
+//
+//  Untitled.swift
+//  TimesMe
+//
+//  Created by Douglas Webb on 4/29/26.
+//
+
+import SwiftUI
+
+struct PlayGame: View{
+    
+    
+    let sentTables :[Int]
+    let numberOfQuestions :Int
+    
+    var body: some View{
+    
+        
+        
+        Text("PlayGame, the input value is \(sentTables)")
+        Text("The number of questions is: \(numberOfQuestions)")
+    }
+    
+    
+}
